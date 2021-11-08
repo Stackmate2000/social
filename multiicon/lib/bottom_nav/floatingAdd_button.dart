67a1adf708830f1e8c541fe755381f47656b0c95
moloi26 <baruahmoloiraj@gmail.com>
2021-11-08 17:48:06 +0530

    addbuuton anim added

--- multiicon/lib/bottom_nav/floatingAdd_button.dart
+++ multiicon/lib/bottom_nav/floatingAdd_button.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 class FloatingAddButton extends StatefulWidget {
   @override
@@ -22,102 +23,38 @@ class _FloatingAddButtonState extends State<FloatingAddButton> {
       padding: const EdgeInsets.only(bottom: 8.0),
       child: Stack(
         children: [
-          Padding(
-            padding: const EdgeInsets.only(bottom: 40.0),
-            child: Align(
-              alignment: Alignment.bottomCenter,
-              child: AnimatedContainer(
-                duration: Duration(milliseconds: 300),
-                curve: Curves.easeIn,
-                width: isOpen ? MediaQuery.of(context).size.width * 0.45 : 0,
-                height: 180,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.all(Radius.circular(21)),
-                  color: Color(0xff3190FF),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(15.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          Text(
-                            "Post a story",
-                            style: GoogleFonts.roboto(
-                                fontSize: 14,
-                                color: Color(0xffFFFFFF),
-                                fontWeight: FontWeight.w500),
-                          ),
-                          Text(
-                            "Upload post",
-                            style: GoogleFonts.roboto(
-                                fontSize: 14,
-                                color: Color(0xffFFFFFF),
-                                fontWeight: FontWeight.w500),
-                          ),
-                          Text(
-                            "Record a reels",
-                            style: GoogleFonts.roboto(
-                                fontSize: 14,
-                                color: Color(0xffFFFFFF),
-                                fontWeight: FontWeight.w500),
-                          ),
-                        ],
+          isOpen
+              ? TweenAnimationBuilder(
+                  duration: Duration(milliseconds: 300),
+                  builder: (BuildContext context, Object _val, Widget child) {
+                    return Padding(
+                      padding: EdgeInsets.only(bottom: _val),
+                      child: child,
+                    );
+                  },
+                  tween: Tween<double>(begin: 50, end: 60),
+                  curve: Curves.easeIn,
+                  child: Align(
+                    alignment: Alignment.bottomCenter,
+                    child: Container(
+                      width:
+                          isOpen ? MediaQuery.of(context).size.width * 0.45 : 0,
+                      height: 60,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(21)),
+                        color: Color(0xffFFFFFF),
                       ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          Container(
-                            decoration: BoxDecoration(
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(8.0)),
-                                color: Color(0xff000000).withOpacity(0.1)),
-                            child: Padding(
-                              padding: const EdgeInsets.all(3.0),
-                              child: Icon(
-                                Icons.ac_unit,
-                                color: Color(0xffFFFFFF),
-                              ),
-                            ),
-                          ),
-                          Container(
-                            decoration: BoxDecoration(
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(8.0)),
-                                color: Color(0xff000000).withOpacity(0.1)),
-                            child: Padding(
-                              padding: const EdgeInsets.all(3.0),
-                              child: Icon(
-                                Icons.access_alarm,
-                                color: Color(0xffFFFFFF),
-                              ),
-                            ),
-                          ),
-                          Container(
-                            decoration: BoxDecoration(
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(8.0)),
-                                color: Color(0xff000000).withOpacity(0.1)),
-                            child: Padding(
-                              padding: const EdgeInsets.all(3.0),
-                              child: Icon(
-                                Icons.tv,
-                                color: Color(0xffFFFFFF),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
+                    ),
+                  ),
+                )
+              : Container(
+                  width: isOpen ? MediaQuery.of(context).size.width * 0.45 : 0,
+                  height: 60,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(21)),
+                    color: Color(0xffFFFFFF),
                   ),
                 ),
-              ),
-            ),
-          ),
           Container(
             alignment: Alignment.bottomCenter,
             child: Positioned(
@@ -126,30 +63,32 @@ class _FloatingAddButtonState extends State<FloatingAddButton> {
                   _toggleOpen();
                 },
                 child: Container(
-                  height: 55,
-                  width: 55,
-                  decoration: BoxDecoration(
-                      boxShadow: [
-                        BoxShadow(
-                            color: Color(0xff46C0DE).withOpacity(0.2),
-                            blurRadius: 4.0,
-                            spreadRadius: 2.0,
-                            offset: Offset(0.0, 6.0))
-                      ],
-                      borderRadius: BorderRadius.all(Radius.circular(21.0)),
-                      gradient: LinearGradient(
-                          colors: [
-                            Color(0xff3190FF),
-                            Color(0xff46C0DE),
-                          ],
-                          begin: Alignment.topCenter,
-                          end: Alignment.bottomCenter)),
-                  child: Icon(
-                    CupertinoIcons.add,
-                    color: Color(0xffFFFFFF),
-                    size: 22,
-                  ),
-                ),
+                    height: 55,
+                    width: 55,
+                    decoration: BoxDecoration(
+                        boxShadow: [
+                          BoxShadow(
+                              color: Color(0xff46C0DE).withOpacity(0.2),
+                              blurRadius: 4.0,
+                              spreadRadius: 2.0,
+                              offset: Offset(0.0, 6.0))
+                        ],
+                        borderRadius: BorderRadius.all(Radius.circular(21.0)),
+                        gradient: LinearGradient(
+                            colors: [
+                              Color(0xff3190FF),
+                              Color(0xff46C0DE),
+                            ],
+                            begin: Alignment.topCenter,
+                            end: Alignment.bottomCenter)),
+                    child: isOpen
+                        ? Icon(MdiIcons.close,
+                            color: Color(0xffFFFFFF), size: 22)
+                        : Icon(
+                            CupertinoIcons.add,
+                            color: Color(0xffFFFFFF),
+                            size: 22,
+                          )),
               ),
             ),
           ),
