d248b181005e99e2ca645de1cf0e0265be62e238
moloi26 <baruahmoloiraj@gmail.com>
2021-11-12 19:15:45 +0530

    profile page updating part gallery

--- multiicon/lib/homescreen_userprofile/tabBar/indexOne.dart
+++ multiicon/lib/homescreen_userprofile/tabBar/indexOne.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class IndexOne extends StatefulWidget {
   @override
@@ -25,6 +25,59 @@ class _IndexOneState extends State<IndexOne> {
                     image: AssetImage("assets/splash.jpg"), fit: BoxFit.cover),
                 color: Colors.grey[600],
               ),
+              child: Stack(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(left: 15.0, top: 15.0),
+                    child: Align(
+                      alignment: Alignment.topLeft,
+                      child: Icon(
+                        CupertinoIcons.heart,
+                        color: Color(0xffFFFFFF),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(left: 15.0, bottom: 15.0),
+                    child: Align(
+                      alignment: Alignment.bottomLeft,
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "31",
+                            style: GoogleFonts.karla(
+                              fontSize: 18.0,
+                              fontWeight: FontWeight.w600,
+                              color: Color(0xffFFFFFF),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 5.0,
+                          ),
+                          Text(
+                            "JAN",
+                            style: GoogleFonts.karla(
+                              fontSize: 14.0,
+                              fontWeight: FontWeight.w600,
+                              color: Color(0xffFFFFFF),
+                            ),
+                          ),
+                          Text(
+                            "2021",
+                            style: GoogleFonts.karla(
+                              fontSize: 14.0,
+                              fontWeight: FontWeight.w600,
+                              color: Color(0xffFFFFFF).withOpacity(0.5),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
           Column(
@@ -39,6 +92,59 @@ class _IndexOneState extends State<IndexOne> {
                       image: AssetImage("assets/girl.jpg"), fit: BoxFit.cover),
                   color: Colors.grey[600],
                 ),
+                child: Stack(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(right: 15.0, top: 15.0),
+                      child: Align(
+                        alignment: Alignment.topRight,
+                        child: Icon(
+                          CupertinoIcons.heart,
+                          color: Color(0xffFFFFFF),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(left: 15.0, bottom: 15.0),
+                      child: Align(
+                        alignment: Alignment.bottomLeft,
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "3",
+                              style: GoogleFonts.karla(
+                                fontSize: 18.0,
+                                fontWeight: FontWeight.w600,
+                                color: Color(0xffFFFFFF),
+                              ),
+                            ),
+                            SizedBox(
+                              height: 5.0,
+                            ),
+                            Text(
+                              "FEB",
+                              style: GoogleFonts.karla(
+                                fontSize: 14.0,
+                                fontWeight: FontWeight.w600,
+                                color: Color(0xffFFFFFF),
+                              ),
+                            ),
+                            Text(
+                              "2021",
+                              style: GoogleFonts.karla(
+                                fontSize: 14.0,
+                                fontWeight: FontWeight.w600,
+                                color: Color(0xffFFFFFF).withOpacity(0.5),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
               Container(
                 height: MediaQuery.of(context).size.height * 0.18,
@@ -48,6 +154,59 @@ class _IndexOneState extends State<IndexOne> {
                   image: DecorationImage(
                       image: AssetImage("assets/work.jpg"), fit: BoxFit.cover),
                   color: Colors.grey[600],
+                ),
+                child: Stack(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(right: 15.0, top: 15.0),
+                      child: Align(
+                        alignment: Alignment.topRight,
+                        child: Icon(
+                          CupertinoIcons.heart,
+                          color: Color(0xffFFFFFF),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(left: 15.0, bottom: 15.0),
+                      child: Align(
+                        alignment: Alignment.bottomLeft,
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "31",
+                              style: GoogleFonts.karla(
+                                fontSize: 18.0,
+                                fontWeight: FontWeight.w600,
+                                color: Color(0xffFFFFFF),
+                              ),
+                            ),
+                            SizedBox(
+                              height: 5.0,
+                            ),
+                            Text(
+                              "JAN",
+                              style: GoogleFonts.karla(
+                                fontSize: 14.0,
+                                fontWeight: FontWeight.w600,
+                                color: Color(0xffFFFFFF),
+                              ),
+                            ),
+                            Text(
+                              "2021",
+                              style: GoogleFonts.karla(
+                                fontSize: 14.0,
+                                fontWeight: FontWeight.w600,
+                                color: Color(0xffFFFFFF).withOpacity(0.5),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ],
