943ad40fab41284aa67032ea7661debb90d23e15
moloi26 <baruahmoloiraj@gmail.com>
2021-11-26 18:28:26 +0530

    profile updating

--- multiicon/lib/homescreen_home/first_image_post/firstImage_post.dart
+++ multiicon/lib/homescreen_home/first_image_post/firstImage_post.dart
@@ -13,8 +13,7 @@ class _FirstImagePostState extends State<FirstImagePost> {
   int counter = 0;
   bool isliked = false;
   bool isfollowing = false;
-  int sharecount = 0;
-  bool isshared = false;
+  bool issavedfile = false;
 
   @override
   Widget build(BuildContext context) {
@@ -145,158 +144,150 @@ class _FirstImagePostState extends State<FirstImagePost> {
                           ),
                         ),
                         SizedBox(
-                          height: 5.0,
+                          height: 10.0,
                         ),
                         Container(
                           width: MediaQuery.of(context).size.width * 0.7,
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
-                              Row(
-                                children: [
-                                  Container(
-                                    child: Icon(
-                                      CupertinoIcons.bubble_middle_bottom,
-                                      size: 20.0,
+                          child: Padding(
+                            padding:
+                                const EdgeInsets.only(left: 15.0, right: 15.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: [
+                                    Row(
+                                      children: [
+                                        Container(
+                                          child: Icon(
+                                            CupertinoIcons.bubble_right,
+                                            size: 20.0,
+                                          ),
+                                        ),
+                                        SizedBox(
+                                          width: 3.0,
+                                        ),
+                                        Text(
+                                          "0",
+                                          style: GoogleFonts.roboto(
+                                              fontSize: 14,
+                                              color: Color(0xff000000)
+                                                  .withOpacity(0.7),
+                                              letterSpacing: 0.2,
+                                              fontWeight: FontWeight.w400),
+                                        )
+                                      ],
                                     ),
-                                  ),
-                                  SizedBox(
-                                    width: 3.0,
-                                  ),
-                                  Text(
-                                    "0",
-                                    style: GoogleFonts.roboto(
-                                        fontSize: 14,
-                                        color:
-                                            Color(0xff000000).withOpacity(0.7),
-                                        letterSpacing: 0.2,
-                                        fontWeight: FontWeight.w400),
-                                  )
-                                ],
-                              ),
-                              Row(
-                                children: [
-                                  Container(
-                                    child: AnimatedCrossFade(
-                                      duration: Duration(milliseconds: 0),
-                                      reverseDuration:
-                                          Duration(milliseconds: 0),
-                                      crossFadeState: isliked
-                                          ? CrossFadeState.showSecond
-                                          : CrossFadeState.showFirst,
-                                      firstChild: GestureDetector(
-                                        onTap: () {
-                                          setState(() {
-                                            counter++;
-                                            {
-                                              print("counter++");
-                                            }
-                                          });
-                                          setState(() {
-                                            isliked = true;
-                                          });
-                                        },
-                                        child: Icon(
-                                          CupertinoIcons.suit_heart,
-                                          size: 20,
-                                        ),
-                                      ),
-                                      secondChild: GestureDetector(
-                                        onTap: () {
-                                          setState(() {
-                                            counter--;
-                                            {
-                                              print("counter--");
-                                            }
-                                          });
-                                          setState(() {
-                                            isliked = false;
-                                          });
-                                        },
-                                        child: Icon(
-                                          CupertinoIcons.suit_heart_fill,
-                                          color: Colors.red,
-                                          size: 20,
-                                        ),
-                                      ),
+                                    SizedBox(
+                                      width: 20.0,
                                     ),
-                                  ),
-                                  SizedBox(
-                                    width: 3.0,
-                                  ),
-                                  Text(
-                                    "$counter",
-                                    style: GoogleFonts.roboto(
-                                        fontSize: 14,
-                                        color:
-                                            Color(0xff000000).withOpacity(0.7),
-                                        letterSpacing: 0.2,
-                                        fontWeight: FontWeight.w400),
-                                  ),
-                                ],
-                              ),
-                              Row(
-                                children: [
-                                  Container(
-                                    child: AnimatedCrossFade(
-                                      duration: Duration(milliseconds: 0),
-                                      reverseDuration:
-                                          Duration(milliseconds: 0),
-                                      crossFadeState: isshared
-                                          ? CrossFadeState.showSecond
-                                          : CrossFadeState.showFirst,
-                                      firstChild: GestureDetector(
-                                        onTap: () {
-                                          setState(() {
-                                            sharecount++;
-                                            {
-                                              print("sharecounter++");
-                                            }
-                                          });
-                                          setState(() {
-                                            isshared = true;
-                                          });
-                                        },
-                                        child: Icon(
-                                          CupertinoIcons.arrow_2_squarepath,
-                                          size: 20,
+                                    Row(
+                                      children: [
+                                        Container(
+                                          child: AnimatedCrossFade(
+                                            duration: Duration(milliseconds: 0),
+                                            reverseDuration:
+                                                Duration(milliseconds: 0),
+                                            crossFadeState: isliked
+                                                ? CrossFadeState.showSecond
+                                                : CrossFadeState.showFirst,
+                                            firstChild: GestureDetector(
+                                              onTap: () {
+                                                setState(() {
+                                                  counter++;
+                                                  {
+                                                    print("counter++");
+                                                  }
+                                                });
+                                                setState(() {
+                                                  isliked = true;
+                                                });
+                                              },
+                                              child: Icon(
+                                                CupertinoIcons.suit_heart,
+                                                size: 20,
+                                              ),
+                                            ),
+                                            secondChild: GestureDetector(
+                                              onTap: () {
+                                                setState(() {
+                                                  counter--;
+                                                  {
+                                                    print("counter--");
+                                                  }
+                                                });
+                                                setState(() {
+                                                  isliked = false;
+                                                });
+                                              },
+                                              child: Icon(
+                                                CupertinoIcons.suit_heart_fill,
+                                                color: Colors.red,
+                                                size: 20,
+                                              ),
+                                            ),
+                                          ),
                                         ),
-                                      ),
-                                      secondChild: GestureDetector(
-                                        onTap: () {
-                                          setState(() {
-                                            sharecount--;
-                                            {
-                                              print("sharecounter--");
-                                            }
-                                          });
-                                          setState(() {
-                                            isshared = false;
-                                          });
-                                        },
-                                        child: Icon(
-                                          CupertinoIcons.arrow_2_squarepath,
-                                          color: Colors.red,
-                                          size: 20,
+                                        SizedBox(
+                                          width: 3.0,
                                         ),
+                                        Text(
+                                          "$counter",
+                                          style: GoogleFonts.roboto(
+                                              fontSize: 14,
+                                              color: Color(0xff000000)
+                                                  .withOpacity(0.7),
+                                              letterSpacing: 0.2,
+                                              fontWeight: FontWeight.w400),
+                                        ),
+                                      ],
+                                    ),
+                                    SizedBox(
+                                      width: 20.0,
+                                    ),
+                                    GestureDetector(
+                                      onTap: () {},
+                                      child: Icon(
+                                        CupertinoIcons.arrow_2_squarepath,
+                                        size: 20,
                                       ),
                                     ),
+                                  ],
+                                ),
+                                Container(
+                                  child: AnimatedCrossFade(
+                                    duration: Duration(milliseconds: 0),
+                                    reverseDuration: Duration(milliseconds: 0),
+                                    crossFadeState: issavedfile
+                                        ? CrossFadeState.showSecond
+                                        : CrossFadeState.showFirst,
+                                    firstChild: GestureDetector(
+                                      onTap: () {
+                                        setState(() {
+                                          issavedfile = true;
+                                        });
+                                      },
+                                      child: Icon(
+                                        CupertinoIcons.bookmark_solid,
+                                        size: 20,
+                                      ),
+                                    ),
+                                    secondChild: GestureDetector(
+                                      onTap: () {
+                                        setState(() {
+                                          issavedfile = false;
+                                        });
+                                      },
+                                      child: Icon(
+                                        CupertinoIcons.bookmark,
+                                        size: 20,
+                                      ),
+                                    ),
                                   ),
-                                  SizedBox(
-                                    width: 3.0,
-                                  ),
-                                  Text(
-                                    "$sharecount",
-                                    style: GoogleFonts.roboto(
-                                        fontSize: 14,
-                                        color:
-                                            Color(0xff000000).withOpacity(0.7),
-                                        letterSpacing: 0.2,
-                                        fontWeight: FontWeight.w400),
-                                  )
-                                ],
-                              ),
-                            ],
+                                ),
+                              ],
+                            ),
                           ),
                         ),
                       ],
--- multiicon/lib/homescreen_home/home_Main.dart
+++ multiicon/lib/homescreen_home/home_Main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:multiicon/homescreen_home/first_image_post/firstImage_post.dart';
 import 'package:multiicon/homescreen_home/multiple_Image_post.dart/multiple_Image_post.dart';
 import 'package:multiicon/homescreen_home/stories/story_Main.dart';
